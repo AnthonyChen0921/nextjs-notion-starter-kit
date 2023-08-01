148dfda3fec8355cc17fc30d19eff07ecc7041d7
1nka <71302562+AnthonyChen0921@users.noreply.github.com>
2023-08-01 00:32:28 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'AnthonyCED1',
   github: 'AnthonyChen0921',
-  linkedin: 'erdong-chen-007aa5249',
+  linkedin: 'anthony-ced',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
